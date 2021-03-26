3697a2ec7adfaf33d68a366e4ec0232e6e54940c
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-03-26 13:27:10 +0100

    create shadow using userPassword from ldap

--- ldif2files.pl
+++ ldif2files.pl
@@ -22,6 +22,19 @@ sub dump_out {
 			$out->{homeDirectory},
 			$out->{loginShell},
 		);
+
+		push @{ $export->{'shadow'} }, join(':',
+			$out->{uid},
+			$out->{password},
+			int( time() / ( 24 * 60 * 60 ) ),	# date of last password change
+			0,		# minimum password age
+			99999,	# maximum password age
+			7,		# password warning period
+			'',	# password inactivity period
+			'',	# account expiration date
+			'',	# reserved field
+		);
+
 	} elsif ( $type eq 'group' ) {
 		my @members;
 		@members = @{ $out->{member} } if $out->{member};
@@ -58,7 +71,17 @@ while(<>) {
 			$out->{$1} = $2;
 		} elsif ( m/(gecos):: (.+)/ ) {
 			$out->{$1} = decode_base64( $2 );
+		} elsif ( m/userPassword:: (.+)/ ) {
+			my $password = $1;
+			if ( length($_) == 79 ) { # line probably continues in next one
+				my $add = <>; chomp($add);
+				if ( $add =~ s{^\s}{} ) { # yes, remove leading space
+					$password .= $add;
+				}
+			}
+			$out->{password} = decode_base64( $password );
 		}
+
 	} elsif ( $type eq 'group' ) {
 		if ( m/(cn|gidNumber): (\S+)/ ) {
 			$out->{$1} = $2;
